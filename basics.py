b4a0c92661634f55d1ec5d999bb2e5b75ee9828e
ADD183 <23123074@pvgcoet.ac.in>
2026-01-07 12:50:34 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,17 @@
+a=5
+b=2.5
+c="Hello"
+d=True
+e=complex(2,3)
+f=[1,2,3]
+g=(4,5,6)
+h={7,8,9}
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))  
+print(type(h))
